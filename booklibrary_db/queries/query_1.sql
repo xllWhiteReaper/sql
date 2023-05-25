d7d20ea83c97d185fd9242c606e6d3666eeade23
A-Cobra <conradbravina@gmail.com>
2023-05-25 18:03:02 -0500

    First 2 queries finished successfully

---
+++ booklibrary_db/queries/query_1.sql
@@ -0,0 +1,8 @@
+/*TASK DESCRIPTION
+Display all contents of the Clients table */
+
+/*Using the current database */
+USE `public_library`;
+
+/*Displaying the contents of the queried table */
+SELECT * FROM `client`;
